6d242817e7df2e0182b8f66bc2982cb57b0070a6
michalshy <91937056+michalshy@users.noreply.github.com>
2024-04-04 21:58:47 +0200

    basic server client communication

--- Reception/Reception.py
+++ Reception/Reception.py
@@ -1,1 +1,32 @@
-print("bardzo")
+import socket  # Import socket module
+
+port = 50000  # Reserve a port for your service every new transfer wants a new port or you must wait.
+s = socket.socket()  # Create a socket object
+host = ""  # Get local machine name
+s.bind(('localhost', port))  # Bind to the port
+s.listen(5)  # Now wait for client connection.
+
+print('Server listening....')
+
+x = 0
+
+while True:
+    conn, address = s.accept()  # Establish connection with client.
+
+    while True:
+        try:
+            print('Got connection from', address)
+            data = conn.recv(1024)
+            print('Server received', data)
+
+            st = 'Thank you for connecting'
+            byt = st.encode()
+            conn.send(byt)
+
+            x += 1
+
+        except Exception as e:
+            print(e)
+            break
+
+conn.close()
--- Transmission/Transmission.py
+++ Transmission/Transmission.py
@@ -1,8 +1,34 @@
-import socket
-import Config
+import socket  # Import socket module
+import os
+import re
 
+s = socket.socket()  # Create a socket object
+port = 50000  # Reserve a port for your service every new transfer wants a new port or you must wait.
 
-class Transmission:
+s.connect(('localhost', port))
+x = 0
 
-    def __init__(self, cfg: Config):
-        self._host = cfg.Config.getHost(cfg)
+st = str(x)
+byt = st.encode()
+s.send(byt)
+
+# send message for hundred times
+while x < 100:
+    st = str(x)
+    byt = st.encode()
+    s.send(byt)
+
+    print(x)
+
+    while True:
+        data = s.recv(1024)
+        if data:
+            print(data)
+            x += 1
+            break
+
+        else:
+            print('no data received')
+
+print('closing')
+s.close()
