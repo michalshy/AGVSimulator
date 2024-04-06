7d90eb23f4ef83cbbe4a8e6b5892a8bcf2fd6456
yumitaru <jakubzajac0222@gmail.com>
2024-04-06 18:52:04 +0200

    Fixes for bad class imports

--- Simulation/Simulation.py
+++ Simulation/Simulation.py
@@ -1,6 +1,11 @@
 import simpy
-from Frame6000 import ENC, NNS, SS
-from Frame6100 import MC, NNC
+from Frame6000.NNS import NNS
+from Frame6000.ENC import ENC
+from Frame6000.SS import SS
+
+from Frame6100.MC import MC
+from Frame6100.NNC import NNC
+
 class AGVSim(object):
     def __init__(self, env):
         self.env = env
@@ -8,8 +13,11 @@ class AGVSim(object):
         # Start the run process everytime an instance is created.
         #self.action = env.process(self.run())
 
+
+    nns = NNS()
     enc = ENC()
     nns = NNS()
     ss = SS()
     mc = MC()
     nnc = NNC()
+
