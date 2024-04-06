a889791f507d38620134bace12984744b288ae61
yumitaru <jakubzajac0222@gmail.com>
2024-04-06 18:26:47 +0200

    Added signals for simulation and divided it into classes

---
+++ Simulation/Frame6100/MC.py
@@ -0,0 +1,10 @@
+class MC:
+    enable = 0  # bit dziala lub nie
+    moveForw = 0
+    moveBack = 0
+    rotLeft = 0
+    rotRight = 0
+    turnLeft = 0
+    turnRight = 0
+
+    velocity = 0.0  # predkosc
--- Simulation/Simulation.py
+++ Simulation/Simulation.py
@@ -1,1 +1,15 @@
-print("jakuba")
+import simpy
+from Frame6000 import ENC, NNS, SS
+from Frame6100 import MC, NNC
+class AGVSim(object):
+    def __init__(self, env):
+        self.env = env
+
+        # Start the run process everytime an instance is created.
+        #self.action = env.process(self.run())
+
+    enc = ENC()
+    nns = NNS()
+    ss = SS()
+    mc = MC()
+    nnc = NNC()
