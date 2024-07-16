3a68cf0010ae5d1e78c72de7bc6d9a83cb0e5d39
michalshy <91937056+michalshy@users.noreply.github.com>
2024-07-16 22:16:26 +0200

    fix

--- Reception/Reception.py
+++ Reception/Reception.py
@@ -5,6 +5,10 @@ from opcua import Client
 
 class Reception:
     def __init__(self, paramManager: ParamManager):
+        self._port = 50000  # placeholder
+        self._sock = socket.socket()
+        self._host = '127.0.0.1'  # placeholder
+        self._data = ''
         self._param_manager = paramManager
         self._url = "opc.tcp://localhost:4841/freeopcua/server/"
         self._nodeId = "ns=2;i="
@@ -16,12 +20,14 @@ class Reception:
 
     def StartReception(self,it):
         self._nodeId += str(it)
-        self.client = Client(self._url)
-        self.client.connect()
-        node = self.client.get_node(self._nodeId)
+        client = Client(self._url)
+        client.connect()
+        node = client.get_node(self._nodeId)
         value = node.get_value()
         self._dataFromServer = value
+
+
         self._nodeId = "ns=2;i=" 
 
     def CloseConnection(self, conn):
-        self.client.disconnect()
+        client.disconnect()
