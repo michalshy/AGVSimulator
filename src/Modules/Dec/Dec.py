216a3c004c5af566f7929d8df6099390fa8d8ed5
Aleksander Sykulski <90504542+Saesenthessis66@users.noreply.github.com>
2024-11-28 19:24:28 +0100

    Add ai module

--- src/Modules/Dec/Dec.py
+++ src/Modules/Dec/Dec.py
@@ -1,20 +1,25 @@
+from NeuralNetwork.AIManager import AI_Manager
+from NeuralNetwork.DataManager import DataManager
+
 # -*- coding: utf-8 -*-
 """Dec module
 
 Deep learning class module, contains method which makes prediction of path possible.
 Navigator module within AGV uses it to checks next points on route based on order.
 """
+
+def AiApiGetRoute(initial_data ,segments_to_traverse):
+    return AI_Manager(20).predict_route(initial_data,segments_to_traverse)
+
 class Dec:
     def __init__(self) -> None:
         self._path = []
-        self._heading = 0
 
-    def PredictPath(self, segments_to_traverse: list): #TODO: PREDICT
-        self._path = ((100,100), (200,200), (300,300), (400,400), (500,500), (600,600))
-        self._heading = 90 
+    def PredictPath(self,initial_data ,segments_to_traverse):
+        dataManager = DataManager('Config/agv.pkl')
+        df = dataManager._fullData[233:254]
+        self._path = AiApiGetRoute(df ,segments_to_traverse)
 
     def ReturnPredictedPath(self):
         return self._path
     
-    def ReturnPredictedHeading(self):
-        return self._heading
